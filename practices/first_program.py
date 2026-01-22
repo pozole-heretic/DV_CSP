4ce8ceafc0af86beff3f8672534227d86db61269
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-01-22 14:32:04 -0700

    finished notes and variables practice

---
+++ practices/first_program.py
@@ -0,0 +1,6 @@
+question = input("What is your favorite color? ")
+
+output = ("That's cool, mine is blue. What is your favorite food? ")
+
+
+print(question, answer)
---
+++ practices/variables_underscore_practice.py
@@ -0,0 +1,24 @@
+#DV Variables Practice
+
+name = "Juan"
+
+number = 7
+
+big_number = 125
+
+food = "pancakes"
+
+color = "blue"
+
+school = "UCAS"
+
+year = 2026
+
+eye_color = "brown"
+
+age = 15
+
+fav_subject = "math"
+
+
+print(name, number, big_number, food, color, school, year, eye_color, age, fav_subject)
