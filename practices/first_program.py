e3ea1246f2854d1e9e4f5fa755e28c012b0f78a7
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-01-23 14:11:21 -0700

    something

--- practices/first_program.py
+++ practices/first_program.py
@@ -1,6 +1,8 @@
 question = input("What is your favorite color? ")
 
-output = ("That's cool, mine is blue. What is your favorite food? ")
+output = input("That's cool, mine is blue. What is your favorite food? ")
+
+question2 = input("Dude, same! I didn't know that we both liked that!")
 
 
-print(question, answer)
+print(question, output, question2)
