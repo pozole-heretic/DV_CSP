25dcbe25913e736aa83aaa04b2a3d589f5f7531c
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-01-23 01:27:10 +0000

    I removed the original code and started from the ground up. I put a variable as a user input and printed it in a sentence.

--- practices/first_program.py
+++ practices/first_program.py
@@ -1,6 +1,4 @@
-question = input("What is your favorite color? ")
-
-output = ("That's cool, mine is blue. What is your favorite food? ")
+name = input("What's your name? ")
 
 
-print(question, answer)
+print("Hello", name)
