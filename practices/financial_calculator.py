5f0466c53ba513787206c3d50c5ab8a8f21ac803
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-01-27 02:30:05 +0000

    finished calculator

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -8,4 +8,16 @@ monthly_groceries = float(input("What is your monthly groceries: $"))
 
 monthly_transportation = float(input("What is your monthly transportation: $"))
 
-print("Your rent is $", monthly_rent_mortgage, "and that is ", round(monthly_rent_mortgage/monthly_income, 2), "% of your income.")
+total = monthly_transportation + monthly_groceries + monthly_utilities + monthly_rent_mortgage
+
+print("Your rent is $", monthly_rent_mortgage, "and that is ", round(monthly_rent_mortgage/monthly_income*100), "% of your income.")
+
+print("Your utilities are $", monthly_utilities, "and that is ", round(monthly_utilities/monthly_income*100), "% of your income.")
+
+print("Your groceries are $", monthly_groceries, "and that is ", round(monthly_groceries/monthly_income*100), "% of your income.")
+
+print("Your transporation is $", monthly_transportation, "and that is ", round(monthly_transportation/monthly_income*100), "% of your income.")
+
+print("You should save $", monthly_income/10, ", that is 10% of your income.")
+
+print("You have $", monthly_income-total, "of spending money each month!")
