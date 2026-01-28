3d630068fec283420e8ebf655a73612e80c9c622
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-01-28 14:31:38 -0700

    finished silly sentences assignment

---
+++ practices/silly_sentences.py
@@ -0,0 +1,15 @@
+#DV period 7 silly_sentences.py
+
+person_1 = input("Tell me a noun (person): ").strip().title()
+
+person_2 = input("Tell me a noun (person): ").strip().title()
+
+thing_1 = input("Tell me a noun (thing): ").strip()
+
+noun_1 = input("Tell me a noun (place): ").strip().title()
+
+noun_2 = input("Tell me a noun (thing)): ").strip()
+
+noun_3 = input("Tell me one last noun (thing): ").strip()
+
+print(person_1 + " went to the beach with " + person_2 + ". First, they swam in their " + thing_1 + ". Then, they saw " + noun_1 + "and " + noun_2 + ". Lastly, they built a sandcastle out of " + noun_3 + ".")
