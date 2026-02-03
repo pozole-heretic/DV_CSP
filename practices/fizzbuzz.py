79248b8504329a97363eb6362adfa50d2e9203bc
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-02-03 14:43:16 -0700

    worked on fizzbuzz

---
+++ practices/fizzbuzz.py
@@ -0,0 +1,23 @@
+start = 0
+
+
+while start <= 50:
+    start += 1
+    if start%15 == 0:
+        print("FizzBuzz")
+    elif start%3 == 0:
+        print("Fizz")
+    elif start%5 == 0:
+        print("Buzz")
+    if start%3 == 0:
+        print("Fizz")
+    else: print(start)
+    
+
+
+"""Loop
+    conditional
+    div by 3
+    div by 5
+    diby 3 and 5
+    number"""
