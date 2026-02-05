9d608cf657cd6d0b26a84f8e5207805e7610139c
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-02-05 15:49:01 -0700

    fixed error

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -11,19 +11,21 @@ monthly_groceries = question("groceries")
 
 monthly_transportation = question("transportation")
 
-total = monthly_transportation + monthly_groceries + monthly_utilities + monthly_rent_mortgage
+savings = monthly_income/10
+
+total = monthly_transportation + monthly_groceries + monthly_utilities + monthly_rent_mortgage + savings
 
 def percent(monthly):
-    return round(monthly/monthly_income)*100
+    return round(monthly/monthly_income*100)
 
 print("Your rent is $", monthly_rent_mortgage, "and that is ", percent(monthly_rent_mortgage), "% of your income.")
 
-"""print("Your utilities are $", monthly_utilities, "and that is ", round(monthly_utilities/monthly_income*100), "% of your income.")
+print("Your utilities are $", monthly_utilities, "and that is ", round(monthly_utilities/monthly_income*100), "% of your income.")
 
 print("Your groceries are $", monthly_groceries, "and that is ", round(monthly_groceries/monthly_income*100), "% of your income.")
 
 print("Your transporation is $", monthly_transportation, "and that is ", round(monthly_transportation/monthly_income*100), "% of your income.")
 
-print("You should save $", monthly_income/10, ", that is 10% of your income.")
+print("You should save $", savings, ", that is 10% of your income.")
 
-print("You have $", monthly_income-total, "of spending money each month!")"""
+print("You have $", monthly_income-total, "of spending money each month!")
