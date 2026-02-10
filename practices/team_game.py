9f28914d86d5b443fc03acf0f451294588df0c8d
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-02-10 14:41:42 -0700

    worked on game
    Co-authored-by: ilikeolives-star <ilikeolives-star@users.noreply.github.com>

--- practices/team_game.py
+++ practices/team_game.py
@@ -1,60 +1,74 @@
 import random
 
-word = ["Rhythm", "Queue", "Sixth", "Jazz", "Gym", "Cryptic"]
+words = ["rhythm", "queue", "sixth", "jazz", "gym", "cryptic"] #this sets a list of words for the computer to choose from
 
-guess = input("What do you think the word is?: ").capitalize()
+word = random.choice(words) #this randomly chooses one word from the list above to use for the game
 
-attempts = 0
+attempts = 0 
 
-if guess in "Rhythm":
+list_of_guessed_letters = []
+
+display_words = ""
+
+guess = input("What do you think the word is?: ").lower()
+
+list_of_guessed_letters.append(guess)
+
+for letter in word:
+    if letter in list_of_guessed_letters:
+         display_words += letter
+    else:
+        display_words += "_"
+
+print(display_words)
+
+if display_words == word:
     print("Congratulations! You guessed the word!")
+if list_of_guessed_letters == guess:
+    print("That was a good guess")
 else:
     attempts += 1
 print(attempts)
 
-user_guess_letters = 
-
-
-print('''
+if attempts == 0:
+    print('''
 ---------
 |       |
 |
 |
 |
 |_________''')
-
-"""---------
-|       |
-|       O
-|
-|
-|_________
-
+elif attempts == 1:
+    print('''
 ---------
 |       |
 |       O
-|       
 |
-|_________
+|
+|_________''')
 
+elif attempts == 2:
+    print('''
 ---------
 |       |
 |       O
 |       |
 |
-|_________
-
+|_________''')
+elif attempts == 3:
+    print('''
 ---------
 |       |
 |       O
 |       |
 |      / \\
-|_________
-
-
+|_________''')
+elif attempts == 4:
+    print('''
 ---------
 |       |
 |       O
 |      /|\\
 |      / \\
-|_________"""
+|_________''')
+print(guess)  
