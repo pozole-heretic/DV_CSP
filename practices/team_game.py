a312a0123834ab59ffc7eccaf1ce4b84ea24e669
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-02-13 01:59:18 +0000

    Finished hangman

--- practices/team_game.py
+++ practices/team_game.py
@@ -1,5 +1,6 @@
-import random
+#DV & HM P7 team_game.py hangman
 
+import random #this imports the random module into the code for it to use, so the code can choose a random word
 
 words = ["rhythm", "queue", "sixth", "jazz", "gym", "cryptic", "abyss", "xylophone", "gypsy", "mnemonic", "buck", "avenue", "injury", "awkward"] #this sets a list of words for the computer to choose from
 
@@ -53,41 +54,40 @@ def hangman(): #this is a function so the game loop can acess it every time by j
 list_of_guessed_letters = [] #this variable is blank because the user inputs will fill this list with letters with their guesses. the code can access this list to print either blank spots, or the letters the user guessed
 
 def game_loop(): #this is a function because after the user wins or loses, it can access this whole loop again depending if they want to play again or not
-    global attempts 
-    while True:
-        global attempts
-        display_words = ""
-        for letter in word:
-            if letter in list_of_guessed_letters:
-                display_words += letter
-            else:
-                display_words += "_"
+    global attempts #attempts is set to global because if it was inside the loop it would be local, and since it's a loop, it would resets attempts to 0 every single time, so it's global on the outside so the number does change if the user guesses wrong
+    while True: #while true loops last forever, so the loop runs every single time and asks the same question to the user
+        display_words = "" #this is currently blank because it depends on what the user guesses, if the user guesses a correct letter, it prints the letter instead a blank spot, along with other blank spots
+        for letter in word: #it compares the letters in the word to the letter that the user guessed
+            if letter in list_of_guessed_letters: #it checks if the letter the user guessed is in the word or not
+                display_words += letter #it prints the letter the user guessed instead of a blank spot if the user guessed a letter right, it would now show the correctly guessed letter possibly along with other blank spots or other correctly guessed letters, adds the letter to the blank display_words variable
+            else: #this is the conditional for if the letter the user guessed is not in the word
+                display_words += "_" #this prints blank spots because the user guessed wrong, it keeps the user guessing, it adds the blank spots to the empty display_words variable
 
-        print(display_words)
+        print(display_words) #it now prints the display words now that it has the info it needed, either blank spots or the correctly guessed letters
 
-        hangman()
+        hangman() #accesses the hangman function, it's a function so it's easier to fit in the loop
 
-        if display_words == word:
-            print("Congratulations! You won the game!")
-            break
-        elif attempts == 4:
-            print("You lost the game! The man was hung! The word was", word)
-            break
-        elif attempts < 4:
-            guess = input("What do you think one of the letters in the word is?: ").lower()
+        if display_words == word: #since display_words adds letters over time, it would check if the display_words is the same as the word the user needs to guess, if all the blank spots are filled it would preform this action, if not, it would go to else
+            print("Congratulations! You won the game!") #it tells the user they won since display_words == word
+            break #it breaks the loop so the user won't be asked any more questions, it would just end the game
+        elif attempts == 4: #since the display_words are not the same as the word, it checks if the attempts are 4, because that's the limit, if it is, it preforms the following:
+            print("You lost the game! The man was hung! The word was", word) #since attempts do = 0, it tells the user they lost and tells them the word they needed to guess
+            break #it breaks the loop so the user can't keep trying to guess after they lost
+        elif attempts < 4: #it checks if attempts are less than 0 if the above elif and if statements are false, because 4 is the losing number, anything less than 4 is still valid for the user to keep guessing
+            guess = input("What do you think one of the letters in the word is?: ").lower() #now it gives the user the ability to guess the word, and the .lower() is so the user can type any letter, uppercase or lowercase because it would just lowercase it anyway so it matches the lowercase of the chosen word because if it was uppercase, it wouldn't match the words since they don't have uppercase letters, so lowercase is so it works
 
-        list_of_guessed_letters.append(guess)
-        if guess not in word:
-            attempts += 1
+        list_of_guessed_letters.append(guess) #it adds the guessed letter to the blank list, therfore giving the code the list to check if the letter is in the word, giving it access to the letter the user guessed
+        if guess not in word: #it's a conditional that checks if the guessed letter was not in the word, if it's true, it preforms the following:
+            attempts += 1 #it adds 1 to attempts since the user guessed wrong, and it gives the hangman loop an instruction to preform depending on the attempts (what hangman to print) and whether the user loses depending on the the number of attempts
 
-game_loop()
+game_loop() #since the above code is all the definition for game_loop(), it finally actually preforms this, it's a variable so the code runner can access it again in case the user wants to play again
 
 play_again = input("Would you like to play again? (yes or no): ").lower() #this input is asking if the user would like to play again
-if play_again == "yes": #if the user chooses to play again, the loop will restart
-    words = ["rhythm", "queue", "sixth", "jazz", "gym", "cryptic"] 
-    word = random.choice(words)
-    attempts = 0
-    list_of_guessed_letters = []
-    game_loop()
-else:
-    print("Thanks for playing!")
+if play_again == "yes": #if the user chooses to play again (which the code checks whether is it yes or no), the loop will restart
+    words = ["rhythm", "queue", "sixth", "jazz", "gym", "cryptic"] #these variables are here to reset the game and reset the variables
+    word = random.choice(words) #this chooses another random word from the list above
+    attempts = 0 #this resets the attempts to 0 because if it didn't, the user would immedietly lose
+    list_of_guessed_letters = [] #this resets the display words to blank spots so the user doesn't know what the word is
+    game_loop() #accesses the function again so the game can run again
+else: #the code does not need to check if the input is no because there are only 2 options, so it doesn't matter what the user says other than yes
+    print("Thanks for playing!") #the end of the whole code!
