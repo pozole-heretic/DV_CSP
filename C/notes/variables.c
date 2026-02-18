b3755ed165d1f7cf64d283885217685c4c9bf5d7
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-02-18 14:41:57 -0700

    started C

---
+++ C/notes/variables.c
@@ -0,0 +1,16 @@
+// This is a comment in C
+#include <stdio.h> // Lets us use inputs and outputs
+
+int main(){
+    // variables examples
+    int age = 9999;
+    float gpa = 3.98;
+    char grade = 'A'; // single letter uses single quotes
+    char name[] = "Eric";
+
+    printf("It worked!\n")
+    printf("%d", age);
+    printf("Your GPA is: %f", gpa);
+    printf("you have a %c in computer science", grade);
+    return 0; // required last line in your main function
+}
