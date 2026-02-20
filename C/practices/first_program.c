36918926a307be407537746cc428d8dfe4514a98
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-02-20 16:36:47 +0000

    worked on first program

--- C/practices/first_program.c
+++ C/practices/first_program.c
@@ -1,5 +1,12 @@
 # include <stdio.h>
 
 int main(){
-    
+    char name[50];
+    char animal[25];
+    printf("What is your name?\n");
+    scanf("%s", &name);
+    printf("What is your favorite animal?\n");
+    scanf("%s", &animal);
+    printf("%s's favorite animal is a(n) %s\n", name, animal);
+    return 0;
 }
