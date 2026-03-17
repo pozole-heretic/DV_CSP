7c055cabc6057573bc9b309f570bd4fefe8314cd
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-03-17 14:41:00 -0600

    done
    Co-authored-by: something7777 <something7777@users.noreply.github.com>

--- C/practices/team_game.c
+++ C/practices/team_game.c
@@ -1,32 +1,72 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int hangman(){
+    int attempts = 0;
     if (attempts = 0){
-        printf("---------\n
-                |        |\n
-                |\n
-                |\n
-                 |\n
-        |__________\n");
+        printf("---------\n");
+        printf("|       |\n");
+        printf("|\n");
+        printf("|\n");
+        printf("|\n");
+        printf("|_________\n");
     }else if (attempts = 1){
-        printf():
+        printf("---------\n");
+        printf("|       |\n");
+        printf("|       O\n");
+        printf("|\n");
+        printf("|\n");
+        printf("|_________\n");
     }else if (attempts = 2){
-        printf();
+        printf("---------\n");
+        printf("|       |\n");
+        printf("|       O\n");
+        printf("|       |\n");
+        printf("|\n");
+        printf("|_________\n");
     }else if (attempts = 3){
-        printf();
+        printf("---------\n");
+        printf("|       |\n");
+        printf("|       O\n");
+        printf("|       |\n");
+        printf("|      / \\\n");
+        printf("|_________\n");
+    }else{
+        printf("---------\n");
+        printf("|       |\n");
+        printf("|       O\n");
+        printf("|      /|\\\n");
+        printf("|      / \\\n");
+        printf("|_________\n");
     }
+    return 0;
 }
 
-
-int game_loop():{
+int game_loop(){
+    //char display_words = "";
 
     return 0;
 }
 
 int main(){
-char[50] = {"rhythm","queue", "sixth", "jazz", "gym", "cryptic", "abyss", "xylophone", 
+    char words[][10] = {"rhythm", "queue", "sixth", "jazz", "gym", "cryptic", "abyss", "xylophone", 
     "gypsy", "mnemonic", "buck", "avenue", "injury", "awkward"};
+        int i = 1;
+    while (i<= 10){
+        printf("%d, ", i);
+        i++;
 
-    return 0;
+   
+        srand(time(NULL));
+
+        printf("\n%d\n", rand() % 14);
+    }
+
+    char word = 
+
+    hangman();
+
+return 0;
 }
