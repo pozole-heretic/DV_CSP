c29d37a9c83b9116c9fe9924fad08198e8d0607a
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-03-18 14:41:11 -0600

    worked on game kinda sorta maybe a wittle bit
    Co-authored-by: something7777 <something7777@users.noreply.github.com>

--- C/practices/team_game.c
+++ C/practices/team_game.c
@@ -2,9 +2,23 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-int hangman(){
-    int attempts = 0;
+char *words[] = {"rhythm", "queue", "sixth", "jazz", "gym", "cryptic", "abyss", "xylophone", "gypsy", "mnemonic", "buck", "avenue", "injury", "awkward"};
+
+int num_words = 14;
+
+char *word;
+
+int attempts = 0;
+
+char list_of_guessed_letters[100];
+
+int guessed_count = 0;
+
+bool won = false;
+
+void hangman(){
     if (attempts = 0){
         printf("---------\n");
         printf("|       |\n");
@@ -41,30 +55,23 @@ int hangman(){
         printf("|      / \\\n");
         printf("|_________\n");
     }
-    return 0;
 }
 
-int game_loop(){
-    //char display_words = "";
 
-    return 0;
+void game_loop(){
+    char guess;
+    while (true){
+        won = true;
+        printf("\n");
+
+        for (int i = 0; i < strlen(word); i++);
+            if(word[i] == list_of_guessed_letters){
+                
+            }
+    };
 }
 
 int main(){
-    char words[][10] = {"rhythm", "queue", "sixth", "jazz", "gym", "cryptic", "abyss", "xylophone", 
-    "gypsy", "mnemonic", "buck", "avenue", "injury", "awkward"};
-        int i = 1;
-    while (i<= 10){
-        printf("%d, ", i);
-        i++;
-
-   
-        srand(time(NULL));
-
-        printf("\n%d\n", rand() % 14);
-    }
-
-    char word = 
 
     hangman();
 
