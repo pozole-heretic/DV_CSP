eea5339e8a92ebb4d247953a844757a4fa7201c8
pozole-heretic <daniel.valenzuela@ucas-edu.net>
2026-03-19 14:42:07 -0600

    done
    Co-authored-by: something7777 <something7777@users.noreply.github.com>

--- C/practices/team_game.c
+++ C/practices/team_game.c
@@ -10,6 +10,10 @@ int num_words = 14;
 
 char *word;
 
+char guess;
+
+char display_words;
+
 int attempts = 0;
 
 char list_of_guessed_letters[100];
@@ -19,28 +23,28 @@ int guessed_count = 0;
 bool won = false;
 
 void hangman(){
-    if (attempts = 0){
+    if (attempts == 0){
         printf("---------\n");
         printf("|       |\n");
         printf("|\n");
         printf("|\n");
         printf("|\n");
         printf("|_________\n");
-    }else if (attempts = 1){
-        printf("---------\n");
-        printf("|       |\n");
-        printf("|       O\n");
-        printf("|\n");
-        printf("|\n");
-        printf("|_________\n");
-    }else if (attempts = 2){
+    }else if (attempts == 1){
         printf("---------\n");
         printf("|       |\n");
         printf("|       O\n");
+        printf("|\n");
+        printf("|\n");
+        printf("|_________\n");
+    }else if (attempts == 2){
+        printf("---------\n");
+        printf("|       |\n");
+        printf("|       O\n");
         printf("|       |\n");
         printf("|\n");
         printf("|_________\n");
-    }else if (attempts = 3){
+    }else if (attempts == 3){
         printf("---------\n");
         printf("|       |\n");
         printf("|       O\n");
@@ -64,15 +68,37 @@ void game_loop(){
         won = true;
         printf("\n");
 
-        for (int i = 0; i < strlen(word); i++);
+        for (int i = 0; i < strlen(word); i++){
+            bool found = false;
             if(word[i] == list_of_guessed_letters){
-                
+                found = true;
+                break;
+            if (found == true){
+                printf("%c", word);
+            }else{
+                printf("_");
+                won = false;
+        }
             }
-    };
+        }
+    }
+
+    if (display_words == word){
+        printf("YAY, YOU WON THE GAME!!!");
+        
+    }else if(attempts == 4){
+        printf("YOU LOST, the words was %d, LOSER\n", words);
+        scanf("%d\n");
+    }else if(attempts < 4){
+        printf("guess a letter for the word please: \n");
+    }
+
+
 }
 
 int main(){
 
+    
     hangman();
 
 return 0;
